3fe136e86aed78d70b180dd57f65ce5d0c1bcf6c
srikanthmunjam143 <srikanthmunjam143@gmail.com>
2025-05-09 15:29:24 +0530

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -31,8 +31,8 @@ from service.models import Product, Category, db
 from service import app
 from tests.factories import ProductFactory
 
-DATABASE_URI = os.getenv(
-    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
+DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///test.db")
+
 )
 
 
